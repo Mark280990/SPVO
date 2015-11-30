4bf2fa545263b747ed048678d8d00830e7d02760
Mark280990 <marco_glez28@outlook.com>
2015-11-29 22:44:48 -0600

    Cambios de generales de vistas

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,18 +29,18 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Inicio', 'url'=>array('/site/index')),
+				array('label'=>'Acerca de?', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'User', 'url'=>array('/user')),
 				array('label'=>'Cliente', 'url'=>array('/cliente')),
 				array('label'=>'Compra', 'url'=>array('/compra')),
 				array('label'=>'Factura', 'url'=>array('/factura')),
-				array('label'=>'Notacompra', 'url'=>array('/notacompra')),
+				array('label'=>'Nota Compra', 'url'=>array('/notacompra')),
 				array('label'=>'Pedido', 'url'=>array('/pedido')),
 				array('label'=>'Pedido Compra', 'url'=>array('/pedido')),
 				array('label'=>'Producto', 'url'=>array('/producto')),
 				array('label'=>'Provedores', 'url'=>array('/provedores')),
+				array('label'=>'Contacto', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
